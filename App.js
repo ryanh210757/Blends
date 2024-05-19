81b8fadb134f88003b507c7b1c4c37be8130ccf3
Ryan Harris <kryanharris1@gmail.com>
2024-05-19 12:47:59 -0500

    Blends project

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {useFonts} from 'expo-font'
 
 export default function App() {
+
+useFonts({
+  Poppy: require('./assets/fonts/Poppins-Bold.ttf')
+})
+const [currentQuestion, setCurrentQuestion] = useState(0)
+
+const questionsArray = [
+  {
+    question: "What type of hair do you have?",
+    choices: ["Thick", "Curly", "Long", "Thin"]
+  },
+  {
+    question: "What is your face shpape?",
+    choices: ["Round", "Square", "Diamond", "Oval"]
+  },
+  {
+    question: "What is your preferred maintenance level?",
+    choices: ["No maintenance", "Low maintenance", "Willing to style"]
+  }
+]
+  let array = []
+const saveAnswer = (answer) => {
+  
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.headerText}>{`
+      FIND 
+      YOUR
+      NEXT
+      HAIRCUT`}</Text>
+      <Text>Need help choosing a haircut?</Text>
+      <View>
+        <Text style={styles.questionText}>
+          {questionsArray[currentQuestion].question}
+        </Text>
+        {questionsArray[currentQuestion].choices.map(item => {
+          return <TouchableOpacity onPress={()=> array.push(item)}style={styles.choicesContainer}>
+                    <Text style={styles.choicesStyle}>{item}</Text>
+                </TouchableOpacity>
+        })}
+  
+        <TouchableOpacity onPress={() =>  currentQuestion + 1 < questionsArray.length ? setCurrentQuestion(currentQuestion + 1) : alert(array) } >
+          <Text>Next</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +58,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  choicesStyle:{
+    color: "green",
+    padding: 5,
+    alignSelf: 'center',
+    fontSize: 18
+  },
+  choicesContainer:{
+    borderColor: 'white',
+    borderWidth: 2,
+    marginTop: 15
+  },
+  questionText:{
+    fontSize: 24,
+    color: 'white'
+  },
+  headerText:{
+    fontSize: 24,
+    color: 'white',
+    fontFamily: 'Poppy',
+    display: 'flex'
+  }
 });
